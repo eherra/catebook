7429a0eb0eae55f8522823aada65b4f5e1ac5d0a
Elias Herranen <elias.herranen@gmail.com>
2020-11-20 15:07:29 +0200

    login page test

---
+++ src/main/java/casebook/DefaultController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casebook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("*")
+    public String handleDefault() {
+        return "login";
+    }
+}
--- src/main/java/casebook/LoginController.java
+++ src/main/java/casebook/LoginController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
     
-    @GetMapping("/login")
+    @GetMapping("/")
     public String loginPage() {
-        return "login";
+        return "login.html";
     }
     
 }
