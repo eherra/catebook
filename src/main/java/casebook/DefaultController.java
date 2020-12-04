edb150fdf020bae1c3b61ab8bf7fd4ffe549f4e0
Elias Herranen <elias.herranen@gmail.com>
2020-12-04 13:38:57 +0200

    fixes

--- src/main/java/casebook/DefaultController.java
+++ src/main/java/casebook/DefaultController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("*")
+    @GetMapping("/")
     public String handleDefault() {
         return "redirect:/frontpage";
     }
